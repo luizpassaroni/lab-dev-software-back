8fa240149c1ad246b76a38aed9696f3242947475
Joao Victor <jovictor.cabral@gmail.com>
2026-06-10 01:34:01 -0300

    remove(src/app.controller.ts): remocao do endpoint @Get(test-error)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,8 +9,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-  @Get('test-error')
-  getError() {
-    throw new Error('erro de teste'); 
-  }
 }
