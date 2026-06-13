388816cd353554bbaae7e2ca182a4c673d9d74d7
Joao Victor <jovictor.cabral@gmail.com>
2026-06-13 16:05:16 -0300

    feat(): feature que adiciona NextPino, pino pretty e global filter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  app.useLogger(app.get(Logger));
 
   app.useGlobalPipes(
     new ValidationPipe({
